d2232574df9e12bbc1709d853080c1c9d2c34e04
Daria Artiomenko <daridaraart@gmail.com>
2023-11-12 18:13:59 +0300

    skills

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () => {
 close.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.tools-tool__item-pros'),
+      lines = document.querySelectorAll('.tools-tool__item-scale span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
